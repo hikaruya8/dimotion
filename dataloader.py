6317dc7a672827656344b4a9804c04735acbcd11
hikaru-y <h.yamada.bg@gmail.com>
2020-01-22 14:47:40 +0900

    前処理で使用するクラスたちの定義done, エラーが出たら改良必要

--- dataloader.py
+++ dataloader.py
@@ -97,75 +97,74 @@ class VideoTransform():
 
 #     # 前処理で使用するクラスたちの定義
 
+class GroupResize():
+    ''' 画像をまとめてリスケールするクラス。
+    画像の短い方の辺の長さがresizeに変換される。
+    アスペクト比は保たれる。
+    '''
 
-# class GroupResize():
-#     ''' 画像をまとめてリスケールするクラス。
-#     画像の短い方の辺の長さがresizeに変換される。
-#     アスペクト比は保たれる。
-#     '''
+    def __init__(self, resize, interpolation=Image.BILINEAR):
+        '''リスケールする処理を用意'''
+        self.rescaler = torchvision.transforms.Resize(resize, interpolation)
 
-#     def __init__(self, resize, interpolation=Image.BILINEAR):
-#         '''リスケールする処理を用意'''
-#         self.rescaler = torchvision.transforms.Resize(resize, interpolation)
-
-#     def __call__(self, img_group):
-#         '''リスケールをimg_group(リスト)内の各imgに実施'''
-#         return [self.rescaler(img) for img in img_group]
-
-
-# class GroupCenterCrop():
-#     ''' 画像をまとめてセンタークロップするクラス。
-#         （crop_size, crop_size）の画像を切り出す。
-#     '''
-
-#     def __init__(self, crop_size):
-#         '''センタークロップする処理を用意'''
-#         self.ccrop = torchvision.transforms.CenterCrop(crop_size)
-
-#     def __call__(self, img_group):
-#         '''センタークロップをimg_group(リスト)内の各imgに実施'''
-#         return [self.ccrop(img) for img in img_group]
-
-# class GroupToTensor():
-#     ''' 画像をまとめてテンソル化するクラス。
-#     '''
-
-#     def __init__(self):
-#         '''テンソル化する処理を用意'''
-#         self.to_tensor = torchvision.transforms.ToTensor()
-
-#     def __call__(self, img_group):
-#         '''テンソル化をimg_group(リスト)内の各imgに実施
-#         0から1ではなく、0から255で扱うため、255をかけ算する。
-#         0から255で扱うのは、学習済みデータの形式に合わせるため
-#         '''
-
-#         return [self.to_tensor(img)*255 for img in img_group]
+    def __call__(self, img_group):
+        '''リスケールをimg_group(リスト)内の各imgに実施'''
+        return [self.rescaler(img) for img in img_group]
 
 
-# class GroupImgNormalize():
-#     ''' 画像をまとめて標準化するクラス。
-#     '''
+class GroupCenterCrop():
+    ''' 画像をまとめてセンタークロップするクラス。
+        （crop_size, crop_size）の画像を切り出す。
+    '''
 
-#     def __init__(self, mean, std):
-#         '''標準化する処理を用意'''
-#         self.normlize = torchvision.transforms.Normalize(mean, std)
+    def __init__(self, crop_size):
+        '''センタークロップする処理を用意'''
+        self.ccrop = torchvision.transforms.CenterCrop(crop_size)
 
-#     def __call__(self, img_group):
-#         '''標準化をimg_group(リスト)内の各imgに実施'''
-#         return [self.normlize(img) for img in img_group]
+    def __call__(self, img_group):
+        '''センタークロップをimg_group(リスト)内の各imgに実施'''
+        return [self.ccrop(img) for img in img_group]
 
-# class Stack():
-#     ''' 画像を一つのテンソルにまとめるクラス。
-#     '''
+class GroupToTensor():
+    ''' 画像をまとめてテンソル化するクラス。
+    '''
 
-#     def __call__(self, img_group):
-#         '''img_groupはtorch.Size([3, 224, 224])を要素とするリスト
-#         '''
-#         ret = torch.cat([(x.flip(dims=[0])).unsqueeze(dim=0)
-#                          for x in img_group], dim=0)  # frames次元で結合
-#         # x.flip(dims=[0])は色チャネルをRGBからBGRへと順番を変えています（元の学習データがBGRであったため）
-#         # unsqueeze(dim=0)はあらたにframes用の次元を作成しています
+    def __init__(self):
+        '''テンソル化する処理を用意'''
+        self.to_tensor = torchvision.transforms.ToTensor()
+
+    def __call__(self, img_group):
+        '''テンソル化をimg_group(リスト)内の各imgに実施
+        0から1ではなく、0から255で扱うため、255をかけ算する。
+        0から255で扱うのは、学習済みデータの形式に合わせるため
+        '''
+
+        return [self.to_tensor(img)*255 for img in img_group]
+
+
+class GroupImgNormalize():
+    ''' 画像をまとめて標準化するクラス。
+    '''
+
+    def __init__(self, mean, std):
+        '''標準化する処理を用意'''
+        self.normlize = torchvision.transforms.Normalize(mean, std)
+
+    def __call__(self, img_group):
+        '''標準化をimg_group(リスト)内の各imgに実施'''
+        return [self.normlize(img) for img in img_group]
+
+class Stack():
+    ''' 画像を一つのテンソルにまとめるクラス。
+    '''
+
+    def __call__(self, img_group):
+        '''img_groupはtorch.Size([3, 224, 224])を要素とするリスト
+        '''
+        ret = torch.cat([(x.flip(dims=[0])).unsqueeze(dim=0)
+                         for x in img_group], dim=0)  # frames次元で結合
+        # x.flip(dims=[0])は色チャネルをRGBからBGRへと順番を変えています（元の学習データがBGRであったため）
+        # unsqueeze(dim=0)はあらたにframes用の次元を作成しています
 
 
 # # Kinetics-400のラベル名をIDに変換する辞書と、逆にIDをラベル名に変換する辞書を用意
