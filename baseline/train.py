9d0641e08b1d518ce1e8734b74dae8df2945c756
hikaru-y <h.yamada.bg@gmail.com>
2020-04-12 16:17:23 +0900

    make baseline folder

---
+++ baseline/train.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+import torchtext
+from torchtext import data
+from torchtext import datasets
+from torchtext.vacab import GloVe
+import torch.nn as nn
+import pickle
+
+
+def __init__(self, path, n_classes, train=True):
+
+
+
