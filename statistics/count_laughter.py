352c0e1236864f61531d13ff4c428a41830188a5
hikaru-y <h.yamada.bg@gmail.com>
2020-04-21 07:23:44 +0900

    change to list

--- statistics/count_laughter.py
+++ statistics/count_laughter.py
@@ -9,8 +9,22 @@ laughter_file = 'dia991_utt7/laugh_0.wav'
 
 with open ('../DialogueRNN/DialogueRNN_features/MELD_features/MELD_features_raw.pkl', 'rb') as f:
     meld_features = pickle.load(f)
-    print(meld_features[2]) #emotion labels:{'neutral': 0, 'surprise': 1, 'fear': 2, 'sadness': 3, 'joy': 4, 'disgust': 5, 'anger': 6}
-    print(meld_features[8]) #sentiment labels: {'neutral': 0, 'positive': 1, 'negative': 2}
+    video_sentences = meld_features[5] #video sentences
+    emotion_labels = meld_features[2]#emotion labels:{'neutral': 0, 'surprise': 1, 'fear': 2, 'sadness': 3, 'joy': 4, 'disgust': 5, 'anger': 6}
+    sentiment_labels = meld_features[8] #sentiment labels: {'neutral': 0, 'positive': 1, 'negative': 2}
+
+# change to list
+sentence_lists = list(video_sentences.values())
+emotion_label_lists = list(emotion_labels.values())
+sentiment_label_lists = list(sentiment_labels.values())
+
+# test
+# print(sentence_lists[0])
+# print(emotion_label_lists[0])
+
+
+
+
 
 # check if detected_train_lauthter_file in folder:
 if os.path.isfile(detected_laughter_folder + laughter_file):
