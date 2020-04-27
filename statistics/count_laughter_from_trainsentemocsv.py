ac5294869493f981d4551624e427846d70fdf2ab
hikaru-y <h.yamada.bg@gmail.com>
2020-04-27 17:51:09 +0900

    現在のsenti done

--- statistics/count_laughter_from_trainsentemocsv.py
+++ statistics/count_laughter_from_trainsentemocsv.py
@@ -11,12 +11,15 @@ detected_folder = '../laughter-detection/detected_train_lauthter/' # checked fol
 # laughter_file = 'dia991_utt7/'
 
 df = pd.read_csv('../MELD/data/MELD/train_sent_emo.csv', header=0)
-video_utterances = meld_features[5]
-emotion_labels = meld_features[2]#emotion labels:{'neutral': 0, 'surprise': 1, 'fear': 2, 'sadness': 3, 'joy': 4, 'disgust': 5, 'anger': 6}
-sentiment_labels = meld_features[8] #sentiment labels: {'neutral': 0, 'positive': 1, 'negative': 2}
+# video_utterances = meld_features[5]
+# emotion_labels = meld_features[2]#emotion labels:{'neutral': 0, 'surprise': 1, 'fear': 2, 'sadness': 3, 'joy': 4, 'disgust': 5, 'anger': 6}
+# sentiment_labels = meld_features[8] #sentiment labels: {'neutral': 0, 'positive': 1, 'negative': 2}
 
 sample = df[(df['Dialogue_ID'] == 0) & (df['Utterance_ID'] == 3)] # sample DialogueID && UtteranceID
 sample_utterance = sample['Utterance'].values.tolist() # sample utterance
+
+
+
 
 
 def functor(f, l): # change str to int function
@@ -25,15 +28,12 @@ def functor(f, l): # change str to int function
   else:
     return f(l)
 
-# change to list
-utterance_lists = list(video_utterances.values())
-emotion_label_lists = list(emotion_labels.values())
-sentiment_label_lists = list(sentiment_labels.values())
-sentiment_label_lists_np = np.array(sentiment_label_lists)
+# # change to list
+# utterance_lists = list(video_utterances.values())
+# emotion_label_lists = list(emotion_labels.values())
+# sentiment_label_lists = list(sentiment_labels.values())
+# sentiment_label_lists_np = np.array(sentiment_label_lists)
 
-# test
-# print(utterance_lists[0])
-# print(emotion_label_lists[0])
 
 # get detected laughter index
 laughter_file_path = glob.glob(detected_folder + '*/laugh_0.wav')
@@ -52,36 +52,37 @@ previous_positive = 0
 previous_negative = 0
 indexerror_sum = 0
 
-for i, l in enumerate(laughter_index):
-    dia_sentiment_index = l[0]
-    utt_sentiment_index = l[1]
-    dia_sentiment_list = sentiment_label_lists[l[0]]
-    try:
-        current_utt_sentiment = dia_sentiment_list[utt_sentiment_index]
 
-        if current_utt_sentiment == 0:
+for i, l in enumerate(laughter_index):
+    dia_index = l[0]
+    utt_index = l[1]
+    current_df =  df[(df['Dialogue_ID'] == dia_index) & (df['Utterance_ID'] == utt_index)]
+    current_utt = current_df['Utterance'].values.tolist()
+    current_senti = current_df['Sentiment'].values.tolist()
+    current_emo = current_df['Emotion'].values.tolist()
+
+    try:
+        if current_senti == ['neutral']:
             current_neutral += 1
-        elif current_utt_sentiment == 1:
+        elif current_senti == ['positive']:
             current_positive += 1
-        elif current_utt_sentiment == 2:
+        elif current_senti == ['negative']:
             current_negative += 1
         else:
             pass
 
-        print('***NOT*** IndexEroor')
-        print("current_dialogue:\n{}".format(video_utterances[dia_sentiment_index]))
-        print('current_utterance:\n{}'.format(video_utterances[dia_sentiment_index][utt_sentiment_index]))
-        print("dia_sentiment_index:{}, utt_sentiment_index:{}, dia_sentiment_list:{}".format(dia_sentiment_index, utt_sentiment_index, dia_sentiment_list))
-        print('\n')
         # check previous sentiment
-        if utt_sentiment_index > 0:
-            previous_utt_sentiment = dia_sentiment_list[utt_sentiment_index-1]
+        if utt_index > 0:
+            previous_df =  df[(df['Dialogue_ID'] == dia_index) & (df['Utterance_ID'] == (utt_index-1))]
+            pre_utt = previous_df['Utterance'].values.tolist()
+            pre_senti = previous_df['Sentiment'].values.tolist()
+            pre_emo = previous_df['Emotion'].values.tolist()
 
-            if previous_utt_sentiment == 0:
+            if pre_senti == ['neutral']:
                 previous_neutral += 1
-            elif previous_utt_sentiment == 1:
+            elif pre_senti == ['positive']:
                 previous_positive += 1
-            elif previous_utt_sentiment == 2:
+            elif pre_senti== ['negative']:
                 previous_negative += 1
             else:
                 pass
@@ -91,16 +92,8 @@ for i, l in enumerate(laughter_index):
 
     except IndexError:
         indexerror_sum += 1
-        print('IndexError')
-        print('laughter_index:{}'.format(l))
-        print('current_dialogue:\n{}'.format(video_utterances[dia_sentiment_index]))
-        print("dia_sentiment_index:{}, utt_sentiment_index:{}, dia_sentiment_list:{} ".format(dia_sentiment_index, utt_sentiment_index, dia_sentiment_list))
-        print('\n')
 
-    if i > 300:
-        break
 
-import pdb;pdb.set_trace()
 
 print("current_neutral:{} \ncurrent_positive:{}, \ncurrent_negative:{}".format(current_neutral, current_positive, current_negative))
 print("IndexError_SUM:{}".format(indexerror_sum))
@@ -108,13 +101,6 @@ X = np.array(['current_neutral', 'current_positive', 'current_negative'])
 Y = np.array([current_neutral, current_positive, current_negative])
 plt.bar(X,Y)
 plt.show()
-
-    # try:
-    #     sentiment_index2.append(sentiment_index[l])
-    # except IndexError:
-    #     continue
-
-    # import pdb;pdb.set_trace()
 
 
 # emotion_index2 = []
